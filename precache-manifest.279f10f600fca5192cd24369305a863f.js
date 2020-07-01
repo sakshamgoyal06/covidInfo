2b36ff092de491fff1dd34d53f62dff6a7e45b03
sakshamgoyal06 <sakshamgoyal06@gmail.com>
2020-07-01 17:42:21 +0530

    Updates

---
+++ precache-manifest.279f10f600fca5192cd24369305a863f.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b731aec92f8c155f2d25dc8681d44dcf",
+    "url": "/covidInfo/index.html"
+  },
+  {
+    "revision": "d50c7afb1ccd13689db1",
+    "url": "/covidInfo/static/css/main.5236a6ef.chunk.css"
+  },
+  {
+    "revision": "d2d4d5189d0ccf3bca27",
+    "url": "/covidInfo/static/js/2.0d1669af.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/covidInfo/static/js/2.0d1669af.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d50c7afb1ccd13689db1",
+    "url": "/covidInfo/static/js/main.d42d010d.chunk.js"
+  },
+  {
+    "revision": "e905778d06e80dc2dcf5",
+    "url": "/covidInfo/static/js/runtime-main.95fe4058.js"
+  }
+]);
